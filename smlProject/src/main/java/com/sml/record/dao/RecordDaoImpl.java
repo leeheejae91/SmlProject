33a31acb0e9d0371439f5129de8c192479a8aedf
leeheejae91 <91moderato@gmail.com>
2015-06-25 14:20:53 +0900

    15.06.25 soccerAllrecord complete

--- smlProject/src/main/java/com/sml/record/dao/RecordDaoImpl.java
+++ smlProject/src/main/java/com/sml/record/dao/RecordDaoImpl.java
@@ -1,11 +1,21 @@
 package com.sml.record.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.sml.record.dto.RecordDto;
 
 @Component
 public class RecordDaoImpl implements RecordDao {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public List<HashMap<String, Object>> getAllRecordList(String sport) {
+		return sqlSession.selectList("dao.RecordMapper.getAllRecordList", sport);
+	}
 }
--- smlProject/src/main/java/com/sml/record/service/RecordServiceImpl.java
+++ smlProject/src/main/java/com/sml/record/service/RecordServiceImpl.java
@@ -12,16 +12,28 @@ import com.sml.member.service.MemberServiceImpl;
 import com.sml.record.dao.RecordDao;
 import com.sml.record.dto.RecordDto;
 
+
 @Service
 public class RecordServiceImpl implements RecordService {
 	private Logger logger = Logger.getLogger(MemberServiceImpl.class.getName());
 	@Autowired
 	private RecordDao recordDao;
 	
+	/**
+	 * @name : RecordServiceImpl
+	 * @date : 2015. 6. 25.
+	 * @author : 이희재
+	 * @description : 축구에 대한 전체 기록을 출력해오기 위함
+	 * 					하나의 dao의 함수를 이용하여 종목만 다르게하여 출력.
+	 */
+	
 	@Override
 	public void soccerGameScore(ModelAndView mav) {
 		HashMap<String, Object> hMap=mav.getModelMap();
 		
-//		List<RecordDto> recordList=recordDao.get
+		List<HashMap<String, Object>> recordList=recordDao.getAllRecordList("축구");
+		logger.info("RecordList : " + recordList.size());
+		mav.addObject("recordList",recordList);
+		mav.setViewName("soccer/soccerGameScore");
 	}
 }
